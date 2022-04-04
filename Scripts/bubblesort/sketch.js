a46cd5c49ead642600e62e3b0dfc36d631350e7e
NBGamer <nabouziyasser80@gmail.com>
2022-04-04 20:41:33 +0000

    add Red indicator for better visualization

--- Scripts/bubblesort/sketch.js
+++ Scripts/bubblesort/sketch.js
@@ -1,22 +1,21 @@
 
 let values = [];
 var i = 0,j = 0;
-// var Arraylength = parseInt(prompt("Give an array value ? (default 100)"));
-var Arraylength = 100;
+var Arraylength = 200;
 
 
 function setup() {
   createCanvas(800, 400);
-  colorMode(HSB,360,200,250);
   values = new Array(Arraylength);
   for(let k = 0; k<values.length;k++){
     values[k] = round(random(height));
   }
+  fill(255);
 }
 
 
 function draw() {
-  background(255);
+  background(0);
   if (values[j] > values[j+1]){
         swap(values,j,j+1);
   }
@@ -29,19 +28,23 @@ function draw() {
     }
   }else{
     console.log("finneshed");
-    // noLoop();
+    noLoop();
   }
   
   for(let l = 0; l<values.length;l++){
-    stroke(255);
-    var colorCycle = frameCount%360;
-    fill(color(colorCycle,100,100));
+    stroke(30);
+    fill(255);
     rect(l*(width/values.length),height-values[l],((l+1)*(width/values.length)-l*(width/values.length)),height);
   }
+  
+  fill(color("red"));
+  rect(j*(width/values.length),height-values[j],((j+1)*(width/values.length)-j*(width/values.length)),height);
+  
 }
 
 function swap(arr,a,b){
   var temp = arr[a];
   arr[a] = arr[b];
   arr[b] = temp;
+  
 }
